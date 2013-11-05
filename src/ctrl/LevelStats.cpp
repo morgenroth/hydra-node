7cee9ff0b1cdbf7c579797935b5dcf9292503452
Johannes Morgenroth <morgenroth@ibr.cs.tu-bs.de>
2013-11-05 09:40:47 +0100

    hnd: Generic stats collection using the /tmp/hydra_stats directory

--- src/ctrl/LevelStats.cpp
+++ src/ctrl/LevelStats.cpp
@@ -9,6 +9,7 @@
 #include "Utils.h"
 #include <ibrcommon/data/File.h>
 #include <sstream>
+#include <fstream>
 
 namespace ctrl {
 
@@ -48,6 +49,32 @@ namespace ctrl {
 					stream << (*it).getBasename() <<
 							" rx:" << Utils::readSysFile((*it).getPath() + "/statistics/rx_bytes") <<
 							" tx:" << Utils::readSysFile((*it).getPath() + "/statistics/tx_bytes") << std::endl;
+				}
+			}
+
+			result_done(stream);
+		}
+		else if (keyword.find("collection") == 0) {
+			result_listing(stream);
+
+			const ibrcommon::File collection_path("/tmp/hnd_stats");
+
+			if (collection_path.isDirectory())
+			{
+				std::list<ibrcommon::File> collection_files;
+				collection_path.getFiles(collection_files);
+
+				for (std::list<ibrcommon::File>::const_iterator it = collection_files.begin(); it != collection_files.end(); ++it)
+				{
+					const ibrcommon::File &f = (*it);
+					if (f.isSystem()) continue;
+
+					stream << "# file: " << f.getBasename() << std::endl;
+
+					// open data file
+					std::ifstream data(f.getPath().c_str());
+
+					stream << data.rdbuf();
 				}
 			}
 
